fe7bebdd43a9f585a8e3d007dc3545aa33268900
Ravikeerthy <98576564+Ravikeerthy@users.noreply.github.com>
2024-01-31 10:32:03 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+var a = 10;
+
+setTimeout(function(){
+    document.getElementById("helltimer").innerHTML = "Your Count down start....";
+
+        setTimeout(function(){
+            document.getElementById("helltimer").innerHTML = a;
+                a--;
+                setTimeout(function(){
+                    document.getElementById("helltimer").innerHTML = a;
+                    a--;
+                    setTimeout(function(){
+                        document.getElementById("helltimer").innerHTML = a;
+                        a--;
+                        setTimeout(function(){
+                            document.getElementById("helltimer").innerHTML = a;
+                            a--;
+                            setTimeout(function(){
+                                document.getElementById("helltimer").innerHTML = a;
+                                a--;
+                                setTimeout(function(){
+                                    document.getElementById("helltimer").innerHTML = a;
+                                    a--;
+                                    setTimeout(function(){
+                                        document.getElementById("helltimer").innerHTML = a;
+                                        a--;
+                                        setTimeout(function(){
+                                            document.getElementById("helltimer").innerHTML = a;
+                                            a--;
+                                            setTimeout(function(){
+                                                document.getElementById("helltimer").innerHTML = a;
+                                                a--;                                                
+                                                setTimeout(function(){
+                                                    document.getElementById("helltimer").innerHTML = a;
+                                                   a--;
+                                                   setTimeout(function(){
+                                                    document.getElementById("helltimer").innerHTML = a;
+                                                   a--;
+                                                   setTimeout(function(){
+                                                    document.getElementById("helltimer").innerHTML = "Happy Independence Day!!";
+                                                //    a--;
+                                                }, 1000);
+                                                }, 1000);
+                                                }, 1000);
+                                            }, 1000);
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        },1000);
+                    },1000);
+
+                },1000);
+    
+    }, 1000);
+    },1000);
